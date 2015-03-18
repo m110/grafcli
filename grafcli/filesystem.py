2dc35ccfd7588dd7c15229b24fe78ae14e09cc1e
m110 <m110@m110.pl>
2015-03-18 22:44:26 +0100

    Add filesystem manager stub.

---
+++ grafcli/filesystem.py
@@ -0,0 +1,22 @@
+import os
+from grafcli.config import config
+
+DATA_DIR = config['filesystem']['data-dir']
+DASHBOARDS_DIR = os.path.join(DATA_DIR, 'dashboards')
+TEMPLATES_DIR = os.path.join(DATA_DIR, 'templates')
+
+
+class FileSystem(object):
+
+    def __init__(self):
+        self._ensure_paths()
+
+    def list_dashboards(self):
+        pass
+
+    def list_templates(self):
+        pass
+
+    def _ensure_paths(self):
+        for path in (DATA_DIR, DASHBOARDS_DIR, TEMPLATES_DIR):
+            os.makedirs(path, mode=0o755, exist_ok=True)
