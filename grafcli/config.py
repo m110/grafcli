0eb12f3f0787d798ac9812c28c3921aa8984bb27
m110 <m110@m110.pl>
2015-03-21 10:51:04 +0100

    Config: expand home directory for config paths.

--- grafcli/config.py
+++ grafcli/config.py
@@ -20,14 +20,16 @@ def load_config(path=None):
 
     config = configparser.ConfigParser()
 
+    paths = DEFAULT_CONFIG_PATHS
     if path:
-        config.read(path)
+        paths.insert(0, path)
+
+    for config_path in DEFAULT_CONFIG_PATHS:
+        config_path = os.path.expanduser(config_path)
+        if os.path.isfile(config_path) and os.access(config_path, os.R_OK):
+            config.read(config_path)
+            break
     else:
-        for config_path in DEFAULT_CONFIG_PATHS:
-            if os.path.isfile(config_path) and os.access(config_path, os.R_OK):
-                config.read(config_path)
-                break
-        else:
-            raise ConfigNotFound("Could not find grafcli.conf")
+        raise ConfigNotFound("Could not find grafcli.conf")
 
     return config
