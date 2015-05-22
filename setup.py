dad3ad5a4c766e3b364186f75f3e0b44f0c9acfa
m110 <m110@m110.pl>
2015-05-22 21:03:23 +0200

    Mark version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='grafcli',
-      version='0.1.0',
+      version='0.2.0',
       description='Grafana CLI management tool',
       author='Milosz Smolka',
       author_email='m110@m110.pl',
