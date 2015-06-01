f9cc5b23228b378471ac34d47bf08d05e4e92f36
m110 <m110@m110.pl>
2015-06-01 21:32:12 +0200

    Improve completer for cp command.

--- grafcli/completer.py
+++ grafcli/completer.py
@@ -3,6 +3,12 @@ from climb.paths import ROOT_PATH, SEPARATOR
 
 
 class GrafCompleter(Completer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._completions.update({
+            'cp': self._cp,
+        })
 
     def _default_completer(self):
         return self._list
@@ -16,3 +22,9 @@ class GrafCompleter(Completer):
                 path = ROOT_PATH + path
 
         return self._cli.commands.ls(path=path).split()
+
+    def _cp(self, source=None, destination=None):
+        if destination:
+            return self._list(destination)
+        else:
+            return self._list(source)
