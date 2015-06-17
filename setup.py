e009eb10dac96b912943d0b7d83c396cd4a9fd92
m110 <m110@m110.pl>
2015-06-17 20:39:20 +0200

    Release 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='grafcli',
-      version='0.3.4',
+      version='0.4.1',
       description='Grafana CLI management tool',
       author='Milosz Smolka',
       author_email='m110@m110.pl',
