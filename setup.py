f02c0c2d8aa7a27a9b336a596befc6f48cb3badb
m110 <m110@m110.pl>
2016-04-20 22:32:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='grafcli',
-      version='0.4.3',
+      version='0.4.5',
       description='Grafana CLI management tool',
       author='Milosz Smolka',
       author_email='m110@m110.pl',
